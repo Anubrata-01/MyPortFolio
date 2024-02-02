4c966055c436c0ae72cf80086cfee4c196b12956
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-02 12:59:40 +0530

    made skills section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,9 +1,11 @@
+import { hover } from '@testing-library/user-event/dist/hover'
 import React from 'react'
 
 const Skills = () => {
   return (
-    <div>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, sunt id. Ipsum ipsam obcaecati recusandae voluptatum adipisci ad molestias, eveniet exercitationem dicta dolores magni, non aliquid cupiditate dolor tempore sapiente dolore, perspiciatis nulla quo impedit sequi. Fuga cum quisquam dolorem dolorum consectetur qui, iure tempora, omnis sunt, porro ratione aperiam! Aspernatur repudiandae porro perferendis quis numquam alias minus eius adipisci natus quasi officia veniam eligendi eos labore, explicabo quae? Perferendis ducimus ex porro laboriosam quod cumque eum est ipsam blanditiis magni, at unde officiis optio ipsum architecto consequuntur totam! Perspiciatis cupiditate nihil minus laudantium voluptate facilis accusantium natus perferendis molestiae.
+    <div className=' pt-8'>
+      <h1 className='text-white w-14 font-bold text-2xl relative left-[48%] mt-8 ' style={{borderBottom:"3px solid red"  }}>Skills</h1>
+     
     </div>
   )
 }
