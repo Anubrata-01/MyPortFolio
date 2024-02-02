49194c98f5044dcc8b30b67b7f428c3bff708fb1
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-02 08:54:14 +0530

    commited and pushed to github

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, sunt id. Ipsum ipsam obcaecati recusandae voluptatum adipisci ad molestias, eveniet exercitationem dicta dolores magni, non aliquid cupiditate dolor tempore sapiente dolore, perspiciatis nulla quo impedit sequi. Fuga cum quisquam dolorem dolorum consectetur qui, iure tempora, omnis sunt, porro ratione aperiam! Aspernatur repudiandae porro perferendis quis numquam alias minus eius adipisci natus quasi officia veniam eligendi eos labore, explicabo quae? Perferendis ducimus ex porro laboriosam quod cumque eum est ipsam blanditiis magni, at unde officiis optio ipsum architecto consequuntur totam! Perspiciatis cupiditate nihil minus laudantium voluptate facilis accusantium natus perferendis molestiae.
+    </div>
+  )
+}
+
+export default Skills
---
+++ src/Utilities/constant.js
@@ -0,0 +1,3 @@
+export const img_url="https://images.pexels.com/photos/951007/pexels-photo-951007.jpeg";
+// https://images.pexels.com/photos/951007/pexels-photo-951007.jpeg?auto=compress&cs=tinysrgb&w=600
+// C:\Users\anubr\Downloads\my.jpg
