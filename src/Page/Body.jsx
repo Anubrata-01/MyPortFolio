fa8d80e987c8ceccdee0c01ef2d701f2d0648f9e
Anubrata-01 <anubratachanda36@gmail.com>
2024-02-02 08:53:11 +0530

    All components are rendered in this Body component

--- src/Page/Body.jsx
+++ src/Page/Body.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Hero from '../Components/Hero';
+import Skills from '../Components/Skills';
+import Education from '../Components/Education';
+import Footer from '../Components/Footer';
 const Body = () => {
    
   return (
-    <div>
+    <div className='w-full h-full relative top-0 flex flex-col justify-between bg-gradient-to-b from-[#BAB7C3] via-[#191B1F] to-[#8D9594] md:w-full md:h-full ' >
         <Navbar/>
-        <h1>hllo</h1>
+        <Hero/>
+        <Skills/>
+        <Education/>
+        <Footer/>
+
     </div>
     
   )
